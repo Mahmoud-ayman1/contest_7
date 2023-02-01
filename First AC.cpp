38404384f17c1b6199f438eee5cf69cfcaf58203
Mahmoud-ayman1 <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-02-01 09:13:51 +0200

    Create First AC.cpp

---
+++ First AC.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n,k;cin>>n>>k;
+    cout<<n/k;
+    return 0;
+}
