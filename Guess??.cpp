fcbbb0fb39dd8bf7e982f0390d60ff0a81d44fe0
Mahmoud-ayman1 <101561030+Mahmoud-ayman1@users.noreply.github.com>
2023-02-01 08:46:58 +0200

    Create Guess??.cpp

---
+++ Guess??.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cout<<"fengqibisheng, yingyueerlai!";
+    return 0;
+}
